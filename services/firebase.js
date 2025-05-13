1c13d51ee5166ed6ba5c308f4302865f523c2304
J7ohn <jvfonseca1349@gmail.com>
2025-05-13 15:26:38 -0300

    configuração do firebase e correção de erro na versão do react-test

---
+++ services/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDi50jcToGCNpCBvQVNA8FpvF3OP3V04mk",
+  authDomain: "gestaoagri.firebaseapp.com",
+  projectId: "gestaoagri",
+  storageBucket: "gestaoagri.firebasestorage.app",
+  messagingSenderId: "657425185675",
+  appId: "1:657425185675:web:aec092e620eb698efd4d8e"
+};
+const app = initializeApp(firebaseConfig);
